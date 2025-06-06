a9a8bec5ade962e1b21e2aeddf3254596a555416
rajVadlamudi <vadlamudiraju@gmail.com>
2025-06-06 14:02:39 +0100

    updated version to 1.0.3

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,1 @@
-var t=()=>"Hello its git tag test ts repo v1.0.2 ";export{t as Greet};
+var t=()=>"Hello its git tag test ts repo v1.0.3";export{t as Greet};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // @ts-check
 export const Greet = ():string=>{
-    return 'Hello its git tag test ts repo v1.0.2 '
+    return 'Hello its git tag test ts repo v1.0.3'
 }
