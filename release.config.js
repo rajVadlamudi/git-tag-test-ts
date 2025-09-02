6ba7ae447da2bdaab8fb7a56c1f8873c21db58e3
rajVadlamudi <vadlamudiraju@gmail.com>
2025-09-02 19:24:00 +0100

    feat: Updated release config

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 export default 
     {
-        "branches": ["main"],
+        "branches": ["release-test"],
         "plugins": [
           "@semantic-release/commit-analyzer",
           "@semantic-release/release-notes-generator",
