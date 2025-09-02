056647eab35e59c7ed2de792ffc01d7708bf17af
rajVadlamudi <vadlamudiraju@gmail.com>
2025-09-02 18:54:26 +0100

    feat: Test semantic-release

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Greet = void 0;
 // @ts-check
 const Greet = () => {
-    return 'Hello its git tag test ts repo v1.0.3';
+    return 'Hello its git tag test ts repo v1.0.4';
 };
 exports.Greet = Greet;
 const message = (0, exports.Greet)();
